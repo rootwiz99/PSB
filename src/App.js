df7800ad72e84d791d8f2ce0ba09d83ad75c5702
rootwiz99 <rutvijbagde1656@gmail.com>
2023-08-07 10:35:39 +0530

    commit99

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const [showModal, setShowModal] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
 
-  const loadingTime = 3000;
+  const loadingTime = 2000;
   const timer = setTimeout(() => {
     setLoading(false);
   }, loadingTime);
